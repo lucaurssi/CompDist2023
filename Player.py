186ee6909eb4f31a4a4b9adea3c30f55bbd834e7
L Urssi <lucaurssi@gmail.com>
2023-12-10 19:11:30 -0300

    session/send message

--- Player.py
+++ Player.py
@@ -4,16 +4,26 @@
 #
 # Jogo Distribuido
 
-import socket
-from threading import Thread
 import os
+import socket
+from threading import Thread, Lock
+from time import sleep
 
 # clears the terminal screen
 def clear(): os.system('cls' if os.name=='nt' else 'clear')
 
 
-def connection(s, conn, addr):# connection to client
+def connection(s, conn, IP):# connection to client
     global in_lobby
+    global player_glossary
+    global player_amount
+    global lock
+    global connected_players
+    failed = False # local
+    
+    conn.settimeout(2)
+    player_amount += 1
+    player = []
     
     with conn: # while inside this, mantain the connection with the client 
         
@@ -21,72 +31,172 @@ def connection(s, conn, addr):# connection to client
         if not in_lobby: 
             return
         
-        # setting nickname for the user
-        nick = conn.recv(1024)
-        nick = nick.decode('ascii')
-        print(f"Connected by {addr} as {nick}")
-        
-        # confirmation
-        data = (f"Hello there {nick}!")
-        conn.sendall(data.encode('utf-8'))
+        try:
+            # setting nickname for the user
+            nick = conn.recv(1024)
+            nick = nick.decode('ascii')
+            
+            player = [nick, IP]
+            if player in player_glossary:
+                print(f"Player {nick} reconnected. {IP}")
+                data = (f"Welcome back {nick}!")
+             
+            else: 
+                player_glossary.append(player)
+                print(f"Connected by {IP} as {nick}")
+                data = (f"Hello there {nick}!")
+            
+            # confirmation
+            conn.sendall(data.encode('utf-8'))
+        except:
+            failed = True
+
+        if not failed:
+            connected_players.append(conn)
         
         # main loop for the connection
-        while True:
-            data = conn.recv(1024) # waits for user input, TO DO: settimeout()
-            if not data:
+        while not failed:
+            try:
+                data = conn.recv(1024) # waits for user input
+                
+            except:
+                failed = True
+                continue
+            
+            if data.decode('ascii') == "ping":
+                continue
+            
+            if not data or data.decode('ascii') == "exit":
                 break
             
-            # logging messages in server
-            print(f"Received: '{data.decode('ascii')}' from user: {nick}")
+            to_send = f"{nick}: {data.decode('ascii')}"
             
-            # checking for command 'play', TO DO: move this to master  
+            # logging messages in server
+            print(to_send)
+            
+            lock.acquire()
+            message.append(to_send) # message will be taken care of by another thread
+            lock.release()
+            
+            
+            # checking for command 'play'
             if in_lobby and data.decode('ascii') == "play" :
                 in_lobby = False
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2: # breaks the master thread loop
                     s2.connect((socket.gethostbyname(socket.gethostname()), PORT))
                     
             
-            # TO DO
-            # send message from here to master if necessary for the game
             
             
-            conn.sendall(data) # confirmation that the message was received
+        
+        if conn in connected_players:
+            connected_players.remove(conn)
         
         # here the connection has closed
-        print(f"User {nick} Disconnected. {addr}")
+        print(f"User {nick} Disconnected. {IP}")
+        player_amount -= 1
+        
+        if in_lobby and player_amount < 1:
+            in_lobby = False
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+                    s2.connect((socket.gethostbyname(socket.gethostname()), PORT))
 
 
+def server_messager():
+    global message # "something"
+    global connected_players # conn
+    global lock
+    
+    while True:
+        if not message:
+            for i in connected_players:
+                i.sendall(("pong").encode('utf-8'))
+            sleep(0.2)
+            continue
+        
+        lock.acquire()
+        to_send = message[0]
+        message.pop(0)
+        lock.release()
+        
+        if message == "die, mailman":
+            break
+        
+        for i in connected_players:
+            i.sendall(to_send.encode('utf-8'))
+        sleep(0.1)
 
 
 def client():
-    nick = input("Your nickname: ")
     global HOST
+    global message
+    global in_lobby
+    global failed
+    
+    nick = input("Your nickname: ")
     HOST = input("Enter the server IP: ")  # The server's hostname or IP address
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(2)
         
-        try:
-            s.settimeout(1)
+        try: # try to connect
             s.connect((HOST, PORT))
-            s.sendall(nick.encode('utf-8'))
-            data = s.recv(1024)
+            s.sendall(nick.encode('utf-8')) # send nick
+            data = s.recv(1024) # Receives back 'Hello there {nick}!'
+            print(data.decode('ascii'))
         except:
             print("Connection to server failed")
+            failed = True
             return
         
+        in_lobby = True # connected
+        
         while(True):
-            message = input("message to send: ")
-            if(message == "exit"):
-                break
-            s.sendall(message.encode('utf-8'))
+            if not message: # wait for message send/ ping pong / receive message
+                try:
+                    s.sendall(("ping").encode('utf-8'))
+                    data = s.recv(1024) # feedback pong
+                except:
+                    print("Connection to server failed. Press 'Enter' to close.")
+                    return
+                
+                if data.decode('ascii') != "pong" and not (f"{nick}: " in data.decode('ascii')):
+                    print(data.decode('ascii'))
+                
+                sleep(0.1)
+                continue
             
-            data = s.recv(1024)
+            if message == "exit":
+                break
+            
+            try:
+                s.sendall(message.encode('utf-8')) # send message
+                message = ""
+                
+                data = s.recv(1024) # feedback
+            except:
+                print("Connection to server failed. Press 'Enter' to close.")
+                failed = True
+                return
+
+
+
+
+
 
 
 HOST = "" # leave empty on server, allows connections from any IP
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 connections = [] # server side threads that mantain connection to clients
+message = ""
+lock = Lock()
 
 in_lobby = True
+failed = False
+
+connected_players = []
+player_glossary = []
+player_amount = 0
 
 clear()
 choice = input(" 1. Host a game \n 2. Join a game \n 3. Exit \n")
@@ -97,26 +207,33 @@ match choice:
         print("<Server>\n")
         print(f"Server IP Address: {socket.gethostbyname(socket.gethostname())}")
         
+        message = []
+        
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
+            
+            mailman = Thread(target=server_messager)
+            mailman.start()
             
             # this loop keep accepting new connections to users
             while in_lobby:
                 s.listen() 
                 conn, addr = s.accept()
-                connections.append(Thread(target=connection, args=(s, conn, addr)))
+                connections.append(Thread(target=connection, args=(s, conn, addr[0])))
                 connections[-1].start()
             
             # a player used the 'play' command a left the loop
             print("Game started")
             
             
-            # TO DO: insert game's server side stuff here
             
             
             # wait for all connections to end, TO DO: force end connections after certain time, timeout ?
             for i in connections:
                 i.join()
+            
+            message.append("die, mailman")
+            mailman.join
         
         print("All players disconnected")
     
@@ -126,8 +243,22 @@ match choice:
         client_thread = Thread(target=client)
         client_thread.start()
         
+        in_lobby = False
         
-        # TO DO: insert game here
+        while(True):
+            if failed: # thread failed to connect or lost connection
+                break
+            
+            if not in_lobby:   # thread still getting name and IP
+                sleep(0.1)
+                continue
+            
+            if message: # wait a bit for the thread to deal with the message
+                sleep(0.1)
+            
+            message = input() # message that the thread will read
+            if message == "exit":
+                break
         
         
         client_thread.join()
